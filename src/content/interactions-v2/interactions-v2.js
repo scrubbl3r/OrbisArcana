f76e3698bf977a41706662a975692317f6612b69
scrubbl3r <79820077+scrubbl3r@users.noreply.github.com>
2026-03-09 21:15:40 -0700

    entity v2 - 38

--- src/content/interactions-v2/interactions-v2.js
+++ src/content/interactions-v2/interactions-v2.js
@@ -12,7 +12,7 @@ export const INTERACTIONS_V2 = Object.freeze({
   defaults: Object.freeze({
     wakeWin: Object.freeze({ ttlMs: 2000 }),
     event: Object.freeze({
-      "event.grace": Object.freeze({ ms: 500 }),
+      "event.grace": Object.freeze({ ms: 5000 }),
     }),
   }),
   rules: Object.freeze([
