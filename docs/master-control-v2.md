9e196929280b5fe64c413ac01277062e8fcce88b
scrubbl3r <79820077+scrubbl3r@users.noreply.github.com>
2026-03-10 20:19:54 -0700

    legacy retirement - 37

--- docs/master-control-v2.md
+++ docs/master-control-v2.md
@@ -1,6 +1,6 @@
 # OrbisArcana Master Control V2
 
-Generated: 2026-03-11T03:16:22.136Z
+Generated: 2026-03-11T03:19:27.979Z
 
 This document is generated from SSOT:
 - spellbook: `src/content/interactions-v2/spellbook-v2.js`
--- src/content/spell-rules/index.js
+++ src/content/spell-rules/index.js
@@ -15,8 +15,7 @@ export {
   EVENT_RUNTIME_BINDINGS_V1,
   EVENT_RUNTIME_BINDINGS_V1_BY_ID,
 } from "./event-runtime-bindings-v1.js";
-export { RULE_ENGINE_V1_MASTER_CONTROL } from "./rule-engine-v1-master-control.js";
-export { RULE_ENGINE_V1_MASTER_CONTROL as RULE_ENGINE_MASTER_CONTROL } from "./rule-engine-v1-master-control.js";
+export { RULE_ENGINE_V1_MASTER_CONTROL, RULE_ENGINE_MASTER_CONTROL } from "./rule-engine-master-control.js";
 export { validateSpellRulesV1, validateSpellRules } from "./validate-spell-rules.js";
 export { validateRuleEngineV1Config, validateRuleEngineConfig } from "./validate-rule-engine-config.js";
 export { buildRuleEngineV1PreviewRuntime, buildRuleEnginePreviewRuntime } from "./build-rule-engine-preview-runtime.js";
