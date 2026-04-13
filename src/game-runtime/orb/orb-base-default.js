692f3baf03acfa92a4ad6e0c2cdb8f68cd827d52
scrubbl3r <79820077+scrubbl3r@users.noreply.github.com>
2026-04-13 15:47:24 -0700

    orb base to 74px test

--- src/game-runtime/orb/orb-base-default.js
+++ src/game-runtime/orb/orb-base-default.js
@@ -1,6 +1,6 @@
 export const ORB_BASE_VISUAL_DEFAULTS = Object.freeze({
-  diameterPx: 100,
-  radiusPx: 50,
+  diameterPx: 74,
+  radiusPx: 37,
   strokeWidthPx: 2,
   strokeDefaultRgb: Object.freeze({
     r: 255,
