713714fcc0477773c02923a136e6b3a8d51d9814
scrubbl3r <79820077+scrubbl3r@users.noreply.github.com>
2026-04-15 17:35:08 -0700

    aoe flame'

--- src/vfx/presets/flame-aoe-default.js
+++ src/vfx/presets/flame-aoe-default.js
@@ -1,6 +1,4 @@
 export const FLAME_AOE_PRESET_DEFAULT = Object.freeze({
-  diameter: 200,
+  diameter: 124,
   durationMs: 10000,
-  stroke: Object.freeze({ r: 255, g: 96, b: 24, a: 1.0 }),
-  fill: Object.freeze({ r: 255, g: 96, b: 24, a: 0.20 }),
 });
