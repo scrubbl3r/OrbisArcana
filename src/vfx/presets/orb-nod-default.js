511649647d00757572c2e7a7b3ea5d0995a19103
scrubbl3r <79820077+scrubbl3r@users.noreply.github.com>
2026-04-15 17:41:38 -0700

    wave depth

--- src/vfx/presets/orb-nod-default.js
+++ src/vfx/presets/orb-nod-default.js
@@ -3,7 +3,7 @@ export const ORB_NOD_PRESET_DEFAULT = Object.freeze({
   orbTemplateDurationMs: 200,
   orbTemplateFillAlpha: 0.07,
   orbTemplateWaveCount: 10,
-  orbTemplateWaveDepthPx: 20.0,
+  orbTemplateWaveDepthPx: 30.0,
   orbTemplateOscillationSpeedHz: 12.0,
   orbTemplateOscillationCount: 2,
 });
