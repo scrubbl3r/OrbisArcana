1fe5e1a51dbaf7189344c389a306bb1e27e891d4
scrubbl3r <79820077+scrubbl3r@users.noreply.github.com>
2026-04-18 11:53:13 -0700

    teleport test

--- src/vfx/presets/teleport-default.js
+++ src/vfx/presets/teleport-default.js
@@ -1,6 +1,6 @@
 export const TELEPORT_PRESET_DEFAULT = Object.freeze({
   orbTeleportFlickerOnMs: 35,
   orbTeleportFlickerOffMs: 35,
-  orbTeleportFadeOutMs: 1000,
-  orbTeleportFadeInMs: 1000,
+  orbTeleportFadeOutMs: 750,
+  orbTeleportFadeInMs: 750,
 });
