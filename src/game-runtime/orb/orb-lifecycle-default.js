4a3ee9e2405b04974d5f8214fdb6b500ee30958f
scrubbl3r <79820077+scrubbl3r@users.noreply.github.com>
2026-04-18 17:42:31 -0700

    lifecycle

--- src/game-runtime/orb/orb-lifecycle-default.js
+++ src/game-runtime/orb/orb-lifecycle-default.js
@@ -1,7 +1,11 @@
 export const ORB_LIFECYCLE_DEFAULTS = Object.freeze({
   maxHits: 3,
   maxShards: 16,
-  shardStrokeRgb: Object.freeze({ r: 255, g: 255, b: 255 }),
+  shardStrokeRgb: Object.freeze({
+    r: 255,
+    g: 255,
+    b: 255,
+  }),
   shardStrokeAlpha: 0.46,
-  shardStrokeWidthPx: 1,
+  shardStrokeWidthPx: 1.00,
 });
