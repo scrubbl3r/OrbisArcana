e592b8626a79823530ec6598db5a94124d9fa5d7
scrubbl3r <79820077+scrubbl3r@users.noreply.github.com>
2026-04-27 12:28:38 -0700

    Refine Orb latitude ripple density

--- labs/world-workshop/world-workshop.js
+++ labs/world-workshop/world-workshop.js
@@ -1,5 +1,5 @@
 import { WORLD_WORKSHOP_SURFACES } from "./world-item-surfaces.js";
-import { createWorldWorkshopPreviewRegistry } from "./world-workshop-preview-registry.js?v=20260427h";
+import { createWorldWorkshopPreviewRegistry } from "./world-workshop-preview-registry.js?v=20260427i";
 import {
   formatWorldWorkshopGeometryReadout,
   formatWorldWorkshopMaterialReadout,
