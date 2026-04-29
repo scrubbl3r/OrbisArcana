60b61337ce43ffc9e0046380e1f0a9fbd51abcc5
scrubbl3r <79820077+scrubbl3r@users.noreply.github.com>
2026-04-29 13:38:18 -0700

    spawn update

--- src/vfx/presets/orb-spawn-default.js
+++ src/vfx/presets/orb-spawn-default.js
@@ -3,5 +3,5 @@ export const ORB_SPAWN_PRESET_DEFAULT = Object.freeze({
   bobSpeedHz: 0.15,
   driftRangeBO: 0.10,
   driftSpeedHz: 0.10,
-  liftReleaseThreshold01: 0.15,
+  liftReleaseThreshold01: 0.25,
 });
