b609d5bf7ce69b0dbc40043b59b7614ffd22ce7e
scrubbl3r <79820077+scrubbl3r@users.noreply.github.com>
2026-05-01 23:48:24 -0700

    nod

--- src/vfx/presets/orb-nod3d-default.js
+++ src/vfx/presets/orb-nod3d-default.js
@@ -1,12 +1,12 @@
 export const ORB_NOD_3D_PRESET_DEFAULT = Object.freeze({
-  orbNod3dShrinkPct: 2,
+  orbNod3dShrinkPct: 6,
   orbNod3dDurationMs: 300,
   orbNod3dFillAlpha: 0.07,
-  orbNod3dWaveCount: 3,
-  orbNod3dLatitudinalBands: 2,
-  orbNod3dWaveDepthBO: 0.054,
+  orbNod3dWaveCount: 1,
+  orbNod3dLatitudinalBands: 1,
+  orbNod3dWaveDepthBO: 0.070,
   orbNod3dOscillationSpeedHz: 9.0,
-  orbNod3dOscillationCount: 3,
+  orbNod3dOscillationCount: 4,
   orbNod3dEquatorFalloff: 0.00,
-  orbNod3dRippleSoftness: 0.82,
+  orbNod3dRippleSoftness: 0.20,
 });
