f8e2a323d0b8518a194c3ecdb592f4785c9e3a5f
scrubbl3r <79820077+scrubbl3r@users.noreply.github.com>
2026-05-05 10:47:01 -0700

    flame physics

--- src/vfx/presets/flame-aoe-3d-default.js
+++ src/vfx/presets/flame-aoe-3d-default.js
@@ -13,7 +13,7 @@ export const FLAME_AOE_3D_PRESET_DEFAULT = Object.freeze({
   "wakeSubdivisions": 64,
   "wakeLeanOffsetBo": 0.5,
   "wakeLeanAmount": 8,
-  "wakeLeanLag": 0.5,
+  "wakeLeanLag": 0.1,
   "wakeLagGraph0Pct": 0,
   "wakeLagGraph0Amount": 0,
   "wakeLagGraph1Pct": 30,
