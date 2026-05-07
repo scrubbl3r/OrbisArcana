3433f86297dd5e6654ee6776f6b802d46b55b1ba
scrubbl3r <79820077+scrubbl3r@users.noreply.github.com>
2026-05-06 20:44:14 -0700

    orbNod

--- src/vfx/presets/orb-nod3d-default.js
+++ src/vfx/presets/orb-nod3d-default.js
@@ -4,7 +4,7 @@ export const ORB_NOD_3D_PRESET_DEFAULT = Object.freeze({
   orbNod3dFillAlpha: 0.07,
   orbNod3dWaveCount: 2,
   orbNod3dLatitudinalBands: 3,
-  orbNod3dWaveDepthBO: 0.050,
+  orbNod3dWaveDepthBO: 0.070,
   orbNod3dOscillationSpeedHz: 12.0,
   orbNod3dOscillationCount: 4,
   orbNod3dEquatorFalloff: 0.00,
