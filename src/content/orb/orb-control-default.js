d6cb3015dae23772ca989c8eac1a12cd971a7d41
scrubbl3r <79820077+scrubbl3r@users.noreply.github.com>
2026-05-10 21:58:28 -0700

    liftboost

--- src/content/orb/orb-control-default.js
+++ src/content/orb/orb-control-default.js
@@ -2,7 +2,7 @@ export const ORB_CONTROL_DEFAULT = Object.freeze({
   fallDragBase: -2.0,
   flatSpinFallDrag: 1.1,
   flatSpinFallDragCurve: 1.0,
-  dynamicLiftBoostStart01: 0.35, // 0.12
-  dynamicLiftBoostMax: 2.5, // 1.55
-  dynamicLiftBoostCurve: 2.5, // 1.1
+  dynamicLiftBoostStart01: 0.40, // 0.12
+  dynamicLiftBoostMax: 2., // 1.55
+  dynamicLiftBoostCurve: 1.5, // 1.1
 });
