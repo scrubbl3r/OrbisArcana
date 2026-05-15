26d946610d05c96e5c8b850963ba828c03bf20ea
scrubbl3r <79820077+scrubbl3r@users.noreply.github.com>
2026-05-15 16:56:32 -0700

    Bust gnat state color imports

--- labs/enemy-workshop/enemy-workshop-preview-registry.js
+++ labs/enemy-workshop/enemy-workshop-preview-registry.js
@@ -1,4 +1,4 @@
-import { renderGnatSwarmPreview } from "./previews/gnat-swarm-preview.js?v=20260515d";
+import { renderGnatSwarmPreview } from "./previews/gnat-swarm-preview.js?v=20260515e";
 
 const PREVIEW_RENDERERS = Object.freeze({
   "gnat-swarm": renderGnatSwarmPreview,
