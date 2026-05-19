2dd77427f2f7d8d9bf26e046b93a33a9f2d58f84
scrubbl3r <79820077+scrubbl3r@users.noreply.github.com>
2026-05-19 09:51:22 -0700

    flame aoe

--- src/game-runtime/behaviors/flame-aoe-behavior-default.js
+++ src/game-runtime/behaviors/flame-aoe-behavior-default.js
@@ -1,6 +1,5 @@
 export const FLAME_AOE_BEHAVIOR_DEFAULT = Object.freeze({
   enabled: true,
-  visualProfile: "spellfire",
   hitRadiusBo: 0.95,
   wakeReachScale: 1.00,
   igniteDamage: 0.10,
--- src/runtime-effects/flame-aoe-3d.js
+++ src/runtime-effects/flame-aoe-3d.js
@@ -1,6 +1,6 @@
 import * as THREE from "three";
 import { disposeThreeObject } from "../game-runtime/rendering/three/three-object-utils.js";
-import { FLAME_AOE_3D_PRESET_DEFAULT } from "../vfx/presets/flame-aoe-3d-default.js?v=20260519094707";
+import { FLAME_AOE_3D_PRESET_DEFAULT } from "../vfx/presets/flame-aoe-3d-default.js?v=20260519095106";
 
 const FLAME_AOE_RENDER_ORDER_BASE = 120;
 
