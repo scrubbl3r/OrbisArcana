8ae5638db69861b5a1b43acbf5cbc836f9faeb61
scrubbl3r <79820077+scrubbl3r@users.noreply.github.com>
2026-05-21 14:47:12 -0700

    Refresh Electric AOE lab preview imports

--- labs/vfx-studio/vfx-studio-bootstrap.js
+++ labs/vfx-studio/vfx-studio-bootstrap.js
@@ -1,4 +1,4 @@
-import { createStudioPreviewRegistry } from "./vfx-studio-preview-registry.js?v=20260521-electric-halo-b";
+import { createStudioPreviewRegistry } from "./vfx-studio-preview-registry.js?v=20260521-electric-halo-c";
 import { createStudioAuthoringAdapters } from "./vfx-studio-adapters.js?v=20260425d";
 import { createStudioSurfaceActivation } from "./vfx-studio-activation.js?v=20260425d";
 
