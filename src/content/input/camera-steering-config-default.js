78e3d771a4f1f3d38a7df2194d32e548825e7c4d
scrubbl3r <79820077+scrubbl3r@users.noreply.github.com>
2026-05-26 20:12:43 -0700

    camera steering speed change

--- src/content/input/camera-steering-config-default.js
+++ src/content/input/camera-steering-config-default.js
@@ -4,6 +4,6 @@ export const CAMERA_STEERING_CONFIG_DEFAULT = Object.freeze({
   centerEpsilon01: 0.002,
   velocityEaseFactor: 0.16,
   maxIntent01: 1.0,
-  maxSpeedPxPerSec: 700,
+  maxSpeedPxPerSec: 1000,
   rampWindow01: 0.75,
 });
