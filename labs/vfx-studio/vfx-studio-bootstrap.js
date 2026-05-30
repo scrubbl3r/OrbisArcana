378bc45ecb0f15d53f8a54347471d8561dfba798
scrubbl3r <79820077+scrubbl3r@users.noreply.github.com>
2026-05-29 19:19:33 -0700

    Revert "Add Flame AOE SDF global source flow"
    This reverts commit baf11c1bf8a26e06e7ff5ec8325f85483356729c.

--- labs/vfx-studio/vfx-studio-bootstrap.js
+++ labs/vfx-studio/vfx-studio-bootstrap.js
@@ -1,4 +1,4 @@
-import { createStudioPreviewRegistry } from "./vfx-studio-preview-registry.js?v=20260529191500";
+import { createStudioPreviewRegistry } from "./vfx-studio-preview-registry.js?v=20260529190000";
 import { createStudioAuthoringAdapters } from "./vfx-studio-adapters.js?v=20260425d";
 import { createStudioSurfaceActivation } from "./vfx-studio-activation.js?v=20260425d";
 
